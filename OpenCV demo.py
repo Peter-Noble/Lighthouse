1bd5c2762aa8a44f7b6bbc72754fee2eb7b49cb9
Ben Hacker <34354459+HackerByName@users.noreply.github.com>
2023-04-14 17:18:59 +0100

    Demo of homography-based CV. Also added example calibration script.

---
+++ OpenCV demo.py
@@ -0,0 +1,117 @@
+import numpy as np
+import helper_functions as fn
+import cv2 as cv
+from os import path
+
+# Attempt to load camera calibration numpy saved files
+calibrations_file_dir = ".\\"
+fn1 = 'calibration_matrix.npy'
+fn2 = 'distortion_coefficients.npy'
+try:
+    camera_matrix = np.load(path.join(calibrations_file_dir, fn1))
+    camera_dist = np.load(path.join(calibrations_file_dir, fn2))
+    print("\n[info] Camera Calibration Matrix Imported:")
+    print(camera_matrix)
+except FileNotFoundError:
+    camera_matrix = None
+    camera_dist = None
+    print("\n[info] Camera Calibration Matrix Could Not Be Imported")
+
+# Define stage geometry in real-world coordinates
+stage_corners_world_geometry = {"upstage-right": [0, 0, 0],
+                                "upstage-left": [12, 0, 0],
+                                "downstage-right": [0, 8, 0],
+                                "downstage-left": [12, 8, 0]}
+stage_corners_pixel_locs = {}
+
+# Instantiate camera and operator window
+print("\n[info] Initialising Camera Input, Please Wait...")
+cap = fn.Camera(1, -2, 1280, 720)
+op_window, w, h = fn.create_display_window("Operator View", (1280, 720))
+print("\n[info] Camera Initialised. Please Follow The Onscreen Instructions.")
+
+# Deal with mouse click positions
+pos_list = []
+
+
+def onMouseDuringSetup(event, x, y, flags, param):
+    global pos_list
+    if event == cv.EVENT_LBUTTONDOWN:
+        print('x = %d, y = %d' % (x, y))
+        pos_list.append((x, y))
+
+
+def onMouseDuringOperation(event, x, y, flags, param):
+    global H
+    if event == cv.EVENT_LBUTTONDOWN:
+        img_pt = [x, y, 1]
+        t_x, t_y, t_z = np.array(np.dot(H, img_pt))
+        target_world_geometry = [t_x / t_z, t_y / t_z, 0]
+        print("\nImage pt: {} in stage coordinate system: \n{}".format((x, y), target_world_geometry))
+
+
+cv.setMouseCallback(op_window, onMouseDuringSetup)
+
+if cap.isOpened():
+    while True:
+        # grab frame from camera
+        ret, frame = cap.read()
+
+        cv.putText(frame, "Please align the camera, then press 'a' to begin", (10, 25),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, 8)
+
+        # display frames to operator
+        cv.imshow(op_window, frame)
+        key = cv.waitKey(1) & 0xFF
+        if key == ord('a'):
+            break
+
+    # Setup stage corner locations
+    for label in stage_corners_world_geometry:
+        while True:
+            print(label)
+            # grab frame from camera
+            ret, frame = cap.read()
+
+            cv.putText(frame, "Select the location of corner '" + label + "' then press 'a'", (10, 25),
+                       cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, 8)
+
+            # display frames to operator
+            cv.imshow(op_window, frame)
+            key = cv.waitKey(0) & 0xFF
+            if key == ord('a'):
+                if len(pos_list) > 0 and pos_list[-1] not in stage_corners_pixel_locs.values():
+                    stage_corners_pixel_locs[label] = pos_list[-1]
+                    break
+            elif key == ord('x') or key == ord('q'):
+                fn.post_display_tidy(cap)
+                exit(0)
+
+    src = np.array(list(stage_corners_pixel_locs.values()), dtype=np.float32)
+    dst = np.array(list(stage_corners_world_geometry.values()), dtype=np.float32)
+
+    # If calibrated camera, account for distortion parameters:
+    if camera_matrix is not None:
+        src = cv.undistortPoints(np.expand_dims(src, axis=1), camera_matrix, camera_dist, None, camera_matrix)
+
+    # Homography relation between real world planar surface of stage and imaging plane (camera sensor)
+    H, _ = cv.findHomography(src, dst)
+    print("\nHomography from img plane to stage system: \n", H)
+
+    cv.setMouseCallback(op_window, onMouseDuringOperation)
+
+    while True:
+
+        # grab frames from camera
+        ret, frame = cap.read()
+
+        cv.putText(frame, "Press 'x' to exit", (10, 25),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, 8)
+
+        # display frames to operator
+        cv.imshow(op_window, frame)
+        key = cv.waitKey(1) & 0xFF
+        if key == ord('x') or key == ord('q'):
+            break
+
+fn.post_display_tidy(cap)
---
+++ helper_functions.py
@@ -0,0 +1,160 @@
+"""
+
+Basic implementations of various CV concepts done the "raw OpenCV" way!
+(IE buffered camera, reading calibration parameter files, converting between camera pixel coordinate system to world)
+
+"""
+
+import os
+import threading
+from threading import Lock
+
+import cv2 as cv
+import numpy as np
+
+
+class Camera:
+    """
+    Class for camera, uses threading to constantly read frames, to prevent buffer filling - which can result in outdated
+    frames being grabbed.
+    https://forum.opencv.org/t/delay-in-videocapture-because-of-buffer/2755 - 'Funny_pangoline'
+    """
+    last_frame = None
+    last_ready = None
+    lock = Lock()
+    capture = None
+    stopped = False
+
+    def __init__(self, device=0, exposure=-4, width=1920, height=1080):
+        self.capture = init_camera_capture(device, exposure, width, height)
+        thread = threading.Thread(target=self.cam_buffer, args=(), name="read_thread")
+        thread.daemon = True
+        thread.start()
+
+    def cam_buffer(self):
+        while not self.stopped:
+            with self.lock:
+                self.last_ready = self.capture.grab()
+
+    def read(self):
+        if self.last_ready is not None:
+            self.last_ready, self.last_frame = self.capture.retrieve()
+            return True, self.last_frame.copy()
+        else:
+            self.stopped = True
+            return False, None
+
+    def release(self):
+        self.stopped = True
+        self.capture.release()
+
+    def isOpened(self):
+        if self.capture is not None and self.stopped is False and self.last_ready is not None:
+            return True
+        return False
+
+
+def get_working_dir():
+    dir = os.getcwd()
+    if len(dir.split('/')) > 1:
+        dir += '/'
+    else:
+        dir += '\\'
+    return dir
+
+
+def get_file_dir():
+    dir = os.path.dirname(os.path.realpath(__file__))
+    if len(dir.split('/')) > 1:
+        dir += '/'
+    else:
+        dir += '\\'
+    return dir
+
+
+def init_camera_capture(device=0, exposure=-5, width=1920, height=1080):
+    """
+    Set up a VideoCapture for use with the specified camera device
+    :param device: (int) index of camera within list of available devices
+    :param exposure: (float) the manual exposure level to set
+    :param width: horizontal resolution
+    :param height: vertical resolution
+    :return: (VideoCapture) the object from which video frames can be read
+    """
+    # Setup capture device
+    cap = cv.VideoCapture(device, apiPreference=cv.CAP_DSHOW)
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    cap.set(cv.CAP_PROP_FOURCC, 0x47504A4D)
+    # cap.set(cv.CAP_PROP_BUFFERSIZE, 1)
+
+    # Prevent auto-exposure and manually set exposure level
+    cap.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.25)
+    cap.set(cv.CAP_PROP_EXPOSURE, float(exposure))
+    cap.set(cv.CAP_PROP_AUTOFOCUS, 0)
+    cap.set(cv.CAP_PROP_AUTO_WB, 0.0)
+    cap.set(cv.CAP_PROP_FPS, 25)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, height)
+    # cap.set(cv.CAP_PROP_SETTINGS, 1)
+
+    return cap
+
+
+# define storage locations
+scripts_file_dir = get_file_dir()
+media_file_dir = scripts_file_dir.replace("scripts", "media")
+calibrations_file_dir = scripts_file_dir.replace("scripts", "calibration_saved_params")
+
+
+def post_display_tidy(cap, out=None):
+    # When everything done, release the capture
+    cap.release()
+    if out is not None:
+        out.release()
+    cv.destroyAllWindows()
+
+
+def create_display_window(name="Stage View", res=(1280, 720), offset=(0, 0)):
+    cv.namedWindow(name, cv.WND_PROP_FULLSCREEN)
+    cv.setWindowProperty(name, cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+    cv.resizeWindow(name, res[0], res[1])
+    cv.moveWindow(name, offset[0], offset[1])
+    blank_img = np.zeros((720, 1280, 3), np.uint8) + 100
+    cv.imshow(name, blank_img)
+    cv.waitKey(1)
+    return name, res[1], res[0]
+
+
+def read_params_from_xml(fn='calibration_result.xml'):
+    path = os.path.join(calibrations_file_dir, fn)
+    fs = cv.FileStorage(path, cv.FILE_STORAGE_READ)
+    cam_int = fs.getNode('cam_int').mat()
+    cam_dist = fs.getNode('cam_dist').mat()
+
+    return cam_int, cam_dist
+
+
+def read_homography_from_xml(fn='homography_result.xml'):
+    path = os.path.join(calibrations_file_dir, fn)
+    fs = cv.FileStorage(path, cv.FILE_STORAGE_READ)
+    h = fs.getNode('h').mat()
+    status = fs.getNode('status').mat()
+
+    return h, status
+
+
+def read_params_from_npy(fn1='calibration_matrix.npy', fn2='distortion_coefficients.npy'):
+    try:
+        calib_mat = np.load(os.path.join(calibrations_file_dir, fn1))
+        dist = np.load(os.path.join(calibrations_file_dir, fn2))
+    except FileNotFoundError:
+        calib_mat = None
+        dist = None
+    print("Camera Calibration Matrix Imported:")
+    print(calib_mat)
+    return calib_mat, dist
+
